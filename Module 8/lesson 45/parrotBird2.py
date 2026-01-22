ed460ee1a5a9bb281c5f44c1e1fef652c0da2e02
agampawar <agampawar09@gmail.com>
2026-01-22 19:18:18 +0530

    changes

--- Module 8/lesson 45/parrotBird2.py
+++ Module 8/lesson 45/parrotBird2.py
@@ -4,9 +4,11 @@ class parrot():
         self.age=age
 
     def sing(self,song):
-        return self.name,"sings ", song
+        # return self.name,"sings ", song
+        return "{} sings {}".format(self.name,song)
     def dance(self):
-        return self.name, "is dancing "
+        # return self.name, "is dancing "
+        return "{} is dancing".format(self.name)
 ob=parrot("koo",5)
 print(ob.sing("happy"))
 print(ob.dance())
